1c9f948d6ecdb5c2fbc6b79d8ccc2da167838ec8
annayedka <anna.yedka@gmail.com>
2022-03-10 14:05:37 +0300

    added tasks

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter the first number: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+double result = Convert.ToDouble(firstNum) / secondNum;
+Console.WriteLine("The result of division: " + result);
